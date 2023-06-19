a60c8e601e71d1e3b23f54435c469c5c20a6e4d9
Safwoen <113114216+Safwoen@users.noreply.github.com>
2023-06-19 15:33:13 +0100

    wall running open

--- My project/Assets/Scripts/WallRunning.cs
+++ My project/Assets/Scripts/WallRunning.cs
@@ -4,22 +4,58 @@ using UnityEngine;
 
 public class WallRunning : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public LayerMask whatIsWall;
+    public float wallrunForce,maxWallrunTime,maxWallSpeed;
+    bool isWallRight,isWallLeft;
+    bool isWallRunning;
+    bool WallClimbing;
+    public Rigidbody rb;
+    public GameObject orientation;
+    public FPSController player;
+   
     void Update()
     {
-        
+        CheckForWall();
+        if (Input.GetKey(KeyCode.D)){StartWallRun();}
+        if (Input.GetKey(KeyCode.A)){StartWallRun();}
     }
-    private void OnCollisionEnter(Collision collision)
+    private void  WallRunInput()
     {
-        if (collision.collider.gameObject.GetComponent<Rigidbody>() == null)
+        if (isWallRight) StartWallRun();
+        if (isWallLeft) StartWallRun();
+    }
+    private void  StartWallRun()
+    {
+        rb.velocity = new Vector3(0, 0, 0);
+        player.gravity = 0;
+        rb.useGravity = false;
+        isWallRunning = true;
+        if(rb.velocity.magnitude <= maxWallSpeed)
         {
-       //  if(Mathf.Abs(Vector3.Dot(collision.GetContact(0).normal, Vector3.up))<0.1f &&(wallLimit != collision .GetContact(0).normal ||(wallLimit != collision.GetContact(0).normal && wallLimitTime <= 0) )
+            rb.AddForce(transform.forward  * wallrunForce * Time.deltaTime);
+
+            if(isWallRight)
+            rb.AddForce(orientation.transform.right  * wallrunForce / 5* Time.deltaTime);
+            else if (isWallLeft)
+            rb.AddForce(-transform.right  * wallrunForce / 5* Time.deltaTime);
+
+        }
+    }
+    private void  StopWallRun()
+    {
+        player.gravity = 10f;
+        rb.useGravity = true;
+        isWallRunning = false;
+    }
+    private void  CheckForWall()
+    {
+        isWallRight = Physics.Raycast(transform.position, orientation.transform.right,1f, whatIsWall);
+        isWallLeft = Physics.Raycast(transform.position, -orientation.transform.right,1f, whatIsWall);
+
+        if(!isWallLeft || ! isWallRight) StopWallRun();
+        if (WallClimbing == true)
+        {
+            
         }
     }
 }
